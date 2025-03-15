e7fbc102517152e25cd28a1a1af0c31292918283
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-03-16 00:11:52 +0530

    added server (app backend) class & minor changes

--- whisper/core/server/response.py
+++ whisper/core/server/response.py
@@ -15,10 +15,4 @@ class Response:
     """A response packet for multiple recipients."""
 
     packet: Packet
-    """Response packet."""
-
     receivers: Iterable[ConnHandle]
-    """Response receivers."""
-
-    def __repr__(self) -> str:
-        return f"<Response: {self.packet!r} to {self.receivers!s}>"

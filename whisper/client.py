a9dce5e689e94fa511a6def07f3b97aa6c96ab48
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-03-15 20:27:53 +0530

    added eventloop and minor changes

--- whisper/client.py
+++ whisper/client.py
@@ -3,43 +3,33 @@ This module contains the mechanism for communicating and action on the
 response sent by the server.
 """
 
-import asyncio
 import logging
-from concurrent.futures import Future, InvalidStateError
 from typing import Coroutine, Set, Any
 
 from .core.packet import Packet
 from .core.client import BaseClient, ClientConn
+from .core.eventloop import EventLoop
 from .settings import Setting
 
 
 logger = logging.getLogger(__name__)
 
 
-class Client(BaseClient):
+class Client(BaseClient, EventLoop):
     """
     The class provides the asynchronous client backend actions for the
-    app. It uses `asyncio` the event loop to manage async tasks.
+    app. It uses `asyncio` event loop to manage async tasks.
     """
 
     def __init__(self, conn: ClientConn | None = None):
         """The `conn` object is used to connect with remote server."""
         BaseClient.__init__(self, conn)
+        EventLoop.__init__(self)
         self.setting = Setting.from_defaults()
-        self.stop_fut: Future[None] = Future()
 
-    @property
-    def loop(self) -> asyncio.AbstractEventLoop:
-        """Asyncio running eventloop."""
-        return asyncio.get_running_loop()
-
-    def schedule(self, task: Coroutine[Any, Any, Any]) -> Future[Any]:
-        """Schedules a task (threadsafe) into event loop."""
-        return asyncio.run_coroutine_threadsafe(task, self.loop)
-
-    def send_packet(self, packet: Packet) -> Future[Any]:
-        """Schedules the packet in the queue."""
-        return self.schedule(self.sendq.put(packet))
+    def send_packet(self, packet: Packet):
+        """Schedules the packet in the outgoing queue."""
+        self.schedule(self.sendq.put(packet))
 
     async def main(self):
         """
@@ -50,21 +40,6 @@ class Client(BaseClient):
         self.connect(host, port)
         await self.process_tasks()
         self.disconnect()
-
-    def stop(self):
-        """Sets the `stop_fut` result to signal for `process_tasks` ending."""
-        try:
-            self.stop_fut.set_result(None)
-        except InvalidStateError:
-            pass
-
-    async def process_tasks(self):
-        """Coroutine handelling the various events."""
-        running_tasks = [asyncio.create_task(task) for task in self.get_tasks()]
-        await asyncio.wrap_future(self.stop_fut)
-        for task in running_tasks:
-            task.cancel()
-        return await asyncio.gather(*running_tasks, return_exceptions=True)
 
     def get_tasks(self) -> Set[Coroutine[Any, Any, Any]]:
         """Provides a set of tasks to start with."""

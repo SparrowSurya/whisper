04dfb99b95bc9ec0e9d4506638b86b6c14283450
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-03-19 00:10:34 +0530

    added workers for client and server

---
+++ whisper/workers.py
@@ -0,0 +1,110 @@
+"""
+This module provides coroutines for client and server.
+"""
+
+import asyncio
+from typing import Awaitable, Callable, Iterable, NoReturn, Sequence, Tuple
+
+from whisper.core.packet import Packet, PacketKind
+from whisper.core.server.client import ConnHandle
+from whisper.utils.coro import handle_cancellation
+
+
+@handle_cancellation("PacketReader")
+async def packet_reader(
+    queue: asyncio.Queue[Packet],
+    reader: Callable[[], Awaitable[Packet]],
+    condition: Callable[[], Awaitable[bool]],
+):
+    """Reads the packet into queue (for client)."""
+    while await condition():
+        packet = await reader()
+        await queue.put(packet)
+
+
+@handle_cancellation("PacketWriter")
+async def packet_writer(
+    queue: asyncio.Queue[Packet],
+    writer: Callable[[Packet], Awaitable[None]],
+    condition: Callable[[], Awaitable[bool]],
+):
+    """Writes the packet from queue (for client)."""
+    while await condition():
+        packet = await queue.get()
+        await writer(packet)
+
+
+@handle_cancellation("PacketHandler")
+async def packet_handler(
+    queue: asyncio.Queue[Packet],
+    handler: Callable[[PacketKind], Callable[[bytes], None]],
+    condition: Callable[[], Awaitable[bool]],
+):
+    """Handles the packet from queue (for client)."""
+    while await condition():
+        packet = await queue.get()
+        handle = handler(packet.kind) # type: ignore
+        if asyncio.iscoroutine(handle):
+            await handle(packet.data)
+        else:
+            handle(packet.data)
+
+
+@handle_cancellation("ConnectionAcceptor")
+async def connection_acceptor(
+    acceptor: Callable[[], Awaitable[ConnHandle]],
+    serve: Callable[[ConnHandle], None],
+    condition: Callable[[], Awaitable[bool]],
+):
+    """Accepts the incoming connections (for server)."""
+    while await condition():
+        conn = await acceptor()
+        serve(conn)
+
+
+@handle_cancellation("ConnectionReader")
+async def connection_reader(
+    conn: ConnHandle,
+    reader: Callable[[ConnHandle], Awaitable[Packet]],
+    queue: asyncio.Queue[Tuple[Packet, ConnHandle]],
+):
+    """Reads the packet from connection (for server)."""
+    while not conn.close:
+        packet = await reader(conn)
+        await queue.put((packet, conn))
+
+
+@handle_cancellation("ConnectionWriter")
+async def connection_writer(
+    conn: ConnHandle,
+    writer: Callable[[Packet, ConnHandle], Awaitable[None]],
+    queue: asyncio.Queue[Tuple[Packet, Iterable[ConnHandle]]],
+):
+    """Reads the packet from connection (for server)."""
+    while not conn.close:
+        packet, conns = await queue.get()
+        for konn in conns:
+            await writer(packet, konn)
+
+
+@handle_cancellation("PacketDispatcher")
+async def packet_dispatcher(
+    recvq: asyncio.Queue[Tuple[Packet, ConnHandle]],
+    sendq: asyncio.Queue[Tuple[Packet, Iterable[ConnHandle]]],
+    handler: Callable[
+        [PacketKind, Tuple[ConnHandle]],
+        Callable[
+            [bytes, ConnHandle],
+            Sequence[Tuple[Packet, Iterable[ConnHandle]]] | None
+        ]
+    ],
+) -> NoReturn:
+    """Handles the incoming packet into outgoing response packet."""
+    while True:
+        packet, conn = await recvq.get()
+        handle = handler(packet.kind) # type: ignore
+        responses = handle(packet.data, conn)
+
+        if responses:
+            for (packet, conns) in responses:
+                await sendq.put((packet, conns))

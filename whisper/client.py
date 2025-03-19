37c756ca50344a44f928126c582b152d134c982c
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-03-19 22:55:15 +0530

    added packet read/write workers to client

--- whisper/client.py
+++ whisper/client.py
@@ -3,11 +3,15 @@ This module contains the client class which can communicate with server
 and perform chat application related functions.
 """
 
+import asyncio
+from functools import cached_property
 import logging
 
-from .core.client import BaseClient, ClientConn
-from .core.eventloop import EventLoop
-from .settings import Setting
+from whisper.core.packet import Packet
+from whisper.core.client import BaseClient, ClientConn
+from whisper.core.eventloop import EventLoop
+from whisper.workers import packet_reader, packet_writer
+from whisper.settings import Setting
 
 
 logger = logging.getLogger(__name__)
@@ -26,6 +30,25 @@ class Client(BaseClient, EventLoop):
         EventLoop.__init__(self)
         self.setting = Setting.from_defaults()
 
+        self.reader = packet_reader(
+            queue=self.recvq,
+            reader=lambda: self.read(self.loop),
+        )
+        self.writer = packet_writer(
+            queue=self.sendq,
+            writer=lambda p: self.write(p, self.loop),
+        )
+
+    @cached_property
+    def recvq(self) -> asyncio.Queue[Packet]:
+        """Packetreceived from server."""
+        return asyncio.Queue()
+
+    @cached_property
+    def sendq(self) -> asyncio.Queue[Packet]:
+        """Packet send to server."""
+        return asyncio.Queue()
+
     async def main(self):
         """
         This defines the entry point of backend. It determines the
@@ -39,3 +62,7 @@ class Client(BaseClient, EventLoop):
         logger.debug("Disconnecting ...")
         self.close_connection()
         logger.debug("Disconnected!")
+
+    def get_tasks(self):
+        """Initial tasks."""
+        return super().get_tasks() | {self.reader, self.writer}

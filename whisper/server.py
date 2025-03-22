635da14ed1c65a9bfdaad1a56a7ccfad6deb7c13
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-03-22 17:06:19 +0530

    added workers to server

--- whisper/server.py
+++ whisper/server.py
@@ -3,10 +3,18 @@ This module provides the server backend class.
 """
 
 import asyncio
+from functools import cached_property
 import logging
+from typing import Iterable, Set, Tuple
 
-from .core.server import BaseServer, ServerConn
-from .core.eventloop import EventLoop
+from whisper.core.server import BaseServer, ServerConn
+from whisper.core.eventloop import EventLoop
+from whisper.workers import (
+    connection_acceptor, connection_reader,
+    connection_writer, packet_dispatcher
+)
+from .core.server.client import ConnHandle
+from .core.packet import Packet
 
 
 logger = logging.getLogger(__name__)
@@ -25,6 +33,34 @@ class Server(BaseServer, EventLoop):
         BaseServer.__init__(self, conn)
         EventLoop.__init__(self)
 
+        self.clients: Set[ConnHandle] = set()
+
+        self.acceptor = connection_acceptor(
+            acceptor=lambda: self.accept(self.loop),
+            serve=lambda conn: self.schedule(self.serve(conn)),
+        )
+
+        self.dispatcher = packet_dispatcher(
+            recvq=self.recvq,
+            sendq=self.sendq,
+            handler=self.handler, # TODO - not implemented
+        )
+
+        self.writer = connection_writer(
+            writer=lambda packet, conn: self.write(conn, packet, self.loop),
+            queue=self.sendq,
+        )
+
+    @cached_property
+    def recvq(self) -> asyncio.Queue[Tuple[Packet, ConnHandle]]:
+        """Packets received from connection."""
+        return asyncio.Queue()
+
+    @cached_property
+    def sendq(self) -> asyncio.Queue[Tuple[Packet, Iterable[ConnHandle]]]:
+        """Packets to be sent to connections."""
+        return asyncio.Queue()
+
     def run(self, host: str, port: int):
         """Starts the server eventloop."""
         try:
@@ -39,3 +75,18 @@ class Server(BaseServer, EventLoop):
         self.start_server(host, port)
         await self.process_tasks()
         self.stop_server()
+
+    async def serve(self, conn: ConnHandle):
+        """Connection lifetime."""
+        logger.info(f"New Connection: {conn.address}")
+        self.clients.add(conn)
+        await connection_reader(
+            conn=conn,
+            reader=lambda conn: self.read(conn, self.loop),
+            queue=self.recvq,
+        )
+        self.clients.remove(conn)
+
+    def get_tasks(self):
+        """Initial tasks."""
+        return super().get_tasks() | {self.acceptor, self.writer}

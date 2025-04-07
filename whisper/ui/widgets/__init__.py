7af3b45cf0c0115a555b001c81bf795e8cc2c787
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-04-07 23:18:02 +0530

    added input widget and typings

--- whisper/ui/widgets/__init__.py
+++ whisper/ui/widgets/__init__.py
@@ -6,10 +6,12 @@ widgets with already built specialized widgets for different parts of the applic
 from .buttons import Button
 from .containers import Container
 from .labels import Label
+from .inputs import Input
 
 
 __all__ = (
     "Button",
     "Container",
     "Label",
+    "Input",
 )

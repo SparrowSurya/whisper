81813aa36e35a651bdc1331fdb098864a4301087
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-04-12 15:52:03 +0530

    Added workers to server backend.

--- whisper/server/workers.py
+++ whisper/server/workers.py
@@ -5,65 +5,69 @@ This module provides coroutines for server.
 import asyncio
 from typing import Awaitable, Callable, Iterable, NoReturn, Sequence, Tuple
 
+from whisper.common.worker import Worker
 from whisper.packet import Packet
 from whisper.packet.v1 import PacketType
 from whisper.server.connection import ConnHandle
-from whisper.common.coro_handler import handle_cancellation
 
 
-@handle_cancellation("ConnectionAcceptor")
-async def connection_acceptor(
-    acceptor: Callable[[], Awaitable[ConnHandle]],
-    serve: Callable[[ConnHandle], None],
-) -> NoReturn:
-    """Accepts the incoming connections (for server)."""
-    while True:
-        conn = await acceptor()
-        serve(conn)
+class ConnAcceptor(Worker):
+    """Accepts incoming connection and serves them."""
+
+    async def work(self,
+        acceptor: Callable[[], Awaitable[ConnHandle]],
+        serve: Callable[[ConnHandle], None],
+    ) -> NoReturn:
+        while True:
+            conn = await acceptor()
+            serve(conn)
 
 
-@handle_cancellation("ConnectionReader")
-async def connection_reader(
-    conn: ConnHandle,
-    reader: Callable[[ConnHandle], Awaitable[Packet]],
-    queue: asyncio.Queue[Tuple[Packet, ConnHandle]],
-):
-    """Reads the packet from connection (for server)."""
-    while not conn.close:
-        packet = await reader(conn)
-        await queue.put((packet, conn))
+class ConnReader(Worker):
+    """Reads incoming packet from connection into queue."""
+
+    async def work(self,
+        conn: ConnHandle,
+        reader: Callable[[ConnHandle], Awaitable[Packet]],
+        queue: asyncio.Queue[Tuple[Packet, ConnHandle]],
+    ):
+        while not conn.close:
+            packet = await reader(conn)
+            await queue.put((packet, conn))
 
 
-@handle_cancellation("ConnectionWriter")
-async def connection_writer(
-    writer: Callable[[Packet, ConnHandle], Awaitable[None]],
-    queue: asyncio.Queue[Tuple[Packet, Iterable[ConnHandle]]],
-) -> NoReturn:
-    """Writes the packet to connections (for server)."""
-    while True:
-        packet, conns = await queue.get()
-        for konn in conns:
-            await writer(packet, konn)
+class ConnWriter(Worker):
+    """Writes the packet to connection from queue"""
+
+    async def work(
+        writer: Callable[[Packet, ConnHandle], Awaitable[None]],
+        queue: asyncio.Queue[Tuple[Packet, Iterable[ConnHandle]]],
+    ) -> NoReturn:
+        while True:
+            packet, conns = await queue.get()
+            for konn in conns:
+                await writer(packet, konn)
 
 
-@handle_cancellation("PacketDispatcher")
-async def packet_dispatcher(
-    recvq: asyncio.Queue[Tuple[Packet, ConnHandle]],
-    sendq: asyncio.Queue[Tuple[Packet, Iterable[ConnHandle]]],
-    handler: Callable[
-        [PacketType, Tuple[ConnHandle]],
-        Callable[
-            [bytes, ConnHandle],
-            Sequence[Tuple[Packet, Iterable[ConnHandle]]] | None
-        ]
-    ],
-) -> NoReturn:
-    """Handles the incoming packet into outgoing response packet."""
-    while True:
-        packet, conn = await recvq.get()
-        handle = handler(packet.kind) # type: ignore
-        responses = handle(packet.data, conn)
+class PacketHandler(Worker):
+    """Handles each request packet and queues its response."""
 
-        if responses:
-            for (packet, conns) in responses:
-                await sendq.put((packet, conns))
+    async def work(
+        recvq: asyncio.Queue[Tuple[Packet, ConnHandle]],
+        sendq: asyncio.Queue[Tuple[Packet, Iterable[ConnHandle]]],
+        handler: Callable[
+            [PacketType, Tuple[ConnHandle]],
+            Callable[
+                [bytes, ConnHandle],
+                Sequence[Tuple[Packet, Iterable[ConnHandle]]] | None
+            ]
+        ],
+    ) -> NoReturn:
+        while True:
+            packet, conn = await recvq.get()
+            handle = handler(packet.kind) # type: ignore
+            responses = handle(packet.data, conn)
+
+            if responses:
+                for (packet, conns) in responses:
+                    await sendq.put((packet, conns))

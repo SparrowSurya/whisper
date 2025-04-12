a1199dcf5e9b48f1537ad9349499e6046d4a2fbf
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-04-12 15:52:03 +0530

    Added workers to server backend.

--- whisper/server/backend.py
+++ whisper/server/backend.py
@@ -4,46 +4,33 @@ This module provides the server backend class.
 
 import asyncio
 from functools import cached_property
-import logging
 from typing import Iterable, Set, Tuple
 
 from whisper.eventloop import EventLoop
 from whisper.packet import Packet
+from whisper.logger import Logger
 from whisper.server.base import BaseServer
 from whisper.server.connection import ConnHandle
 from whisper.server.tcp import  TcpServer
-from whisper.server.workers import (connection_acceptor, connection_reader,
-    connection_writer, packet_dispatcher)
-
-
-logger = logging.getLogger(__name__)
+from whisper.server.workers import ConnAcceptor, ConnReader, ConnWriter, PacketHandler
 
 
 class Server(BaseServer, EventLoop):
     """This class provides asynchronouse server backend for the chat applications."""
 
-    def __init__(self, conn: TcpServer | None = None):
+    def __init__(self, logger: Logger, conn: TcpServer | None = None):
         """The connection object is used to accept client connections."""
         BaseServer.__init__(self, conn)
         EventLoop.__init__(self)
 
+        self.logger = logger
         self.clients: Set[ConnHandle] = set()
 
-        self.acceptor = connection_acceptor(
-            acceptor=lambda: self.accept(self.loop),
-            serve=lambda conn: self.schedule(self.serve(conn)),
-        )
+        self.acceptor = ConnAcceptor(logger=self.logger)
+        self.handler = PacketHandler(logger=self.logger)
+        self.writer = ConnWriter(logger=self.logger)
 
-        self.dispatcher = packet_dispatcher(
-            recvq=self.recvq,
-            sendq=self.sendq,
-            handler=self.handler, # TODO - not implemented
-        )
-
-        self.writer = connection_writer(
-            writer=lambda packet, conn: self.write(conn, packet, self.loop),
-            queue=self.sendq,
-        )
+        self.handlers = {} # TODO: packet handlers
 
     @cached_property
     def recvq(self) -> asyncio.Queue[Tuple[Packet, ConnHandle]]:
@@ -59,22 +46,23 @@ class Server(BaseServer, EventLoop):
         """Starts the server eventloop."""
         try:
             asyncio.run(self.start(host, port))
-        except KeyboardInterrupt:
-            logger.info("Force stopping the server!")
-        except Exception as ex:
-            logger.exception(ex)
+        except BaseException as ex:
+            self.logger.exception(str(ex))
 
     async def start(self, host: str, port: int):
         """Start the server."""
         self.start_server(host, port)
+        self.logger.info(f"server running at {(host, port)}")
         await self.execute()
         self.stop_server()
+        self.logger.info("server stopped")
 
     async def serve(self, conn: ConnHandle):
         """Connection lifetime."""
-        logger.info(f"New Connection: {conn.address}")
+        self.logger.info(f"new connection from {conn.address}")
         self.clients.add(conn)
-        await connection_reader(
+        reader = ConnReader(logger=self.logger)
+        await reader(
             conn=conn,
             reader=lambda conn: self.read(conn, self.loop),
             queue=self.recvq,
@@ -83,4 +71,18 @@ class Server(BaseServer, EventLoop):
 
     def initial_tasks(self):
         """Initial tasks."""
-        return super().initial_tasks() | {self.acceptor, self.writer}
+        return super().initial_tasks() | {
+            self.acceptor(
+                acceptor=lambda: self.accept(self.loop),
+                serve=lambda conn: self.schedule(self.serve(conn)),
+            ),
+            self.writer(
+                writer=lambda packet, conn: self.write(conn, packet, self.loop),
+                queue=self.sendq,
+            ),
+            self.handler(
+                recvq=self.recvq,
+                sendq=self.sendq,
+                handler=self.get_handler, # TODO - not implemented
+            ),
+        }

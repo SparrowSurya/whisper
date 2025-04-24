a396919845b7ddd0cfe4b270a4d62197c90aebc2
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-04-25 00:07:52 +0530

    Added forms to ui.

---
+++ whisper/ui/form/__init__.py
@@ -0,0 +1,10 @@
+"""
+This package provides form and related widgets.
+"""
+
+from .form import Form  # noqa: F401
+from .labels import FormLabel, FormErrorLabel  # noqa: F401
+from .inputs import FormTextInput  # noqa: F401
+from .actions import FormSubmit, FormCancel, FormReset  # noqa: F401
+from .base import FormInput, FormAction, FormActionType  # noqa: F401
+from .groups import FormTextInputGroup  # noqa: F401

2f2ed5a95355f531f4971e4a6d7b25a0de9cc015
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-04-29 22:56:04 +0530

    EventLoop modified to handle signals. Fixed wrong type annotations. Added new method to EventLoop.

--- whisper/eventloop.py
+++ whisper/eventloop.py
@@ -1,32 +1,50 @@
 """
-This module contains the mechanism for communicating and action on the response sent
-by the server.
+This module contains the eventloop to manage the running tasks and coroutines with
+graceful exit.
 """
 
+import sys
+import signal
 import asyncio
+import threading
 from concurrent.futures import Future
-from typing import Coroutine, List, Dict, Set, Any
+from typing import Tuple, List, Dict, Set, Any, Callable, Coroutine, ParamSpec, TypeVar
 
+
+P = ParamSpec("P")
+R = TypeVar("R")
 
 class EventLoop:
-    """The class provides the asynchronous eventloop using `asyncio`. It provides
-    methods to control the running eventloop.
+    """
+    The class provides the asynchronous eventloop using `asyncio`. It handles
+    exceptions and signals for graceful exit.
 
     **Attributes & Properties**:
     * loop - asyncio eventloop
+    * signals - signals that will be handled by the eventloop
+    * CancelledError - error raised when coroutine is cancelled
 
     **Methods**:
-    * run_coro - run coroutine
-    * exectue - executes the eventloop unill stopped
+    * run_main - run coroutine
+    * main - coroutine between setup and teardown
     * schedule - schedule a task from another thread
     * create_task - create a task (must be called from same thread)
-    * stop_eventloop - stop the eventloop
+    * stop_main - finish the execution of `main` coroutine above
+    * signal_handler - handles the received signal to the process
+    * exception_handler - handle uncaught exception in eventloop tasks
     * initial_tasks - any task that need to run as soon as eventloop starts
-    * exception_handler - handle exception in asyncio tasks
     """
 
+    signals: List[int] = [signal.SIGINT, signal.SIGTERM]
+    if sys.platform == "win32":
+        signals.extend((signal.SIGBREAK, signal.CTRL_C_EVENT, signal.CTRL_BREAK_EVENT))
+    else:
+        signals.append(signal.SIGQUIT)
+
+    CancelledError = asyncio.CancelledError
+
     def __init__(self):
-        self._stop_event = asyncio.Event()
+        self._stop_event = Future()
         self._loop = asyncio.get_event_loop()
 
     @property
@@ -34,26 +52,38 @@ class EventLoop:
         """Eventloop instance."""
         return self._loop
 
-    def run_coro(self, coro: Coroutine[None, None, Any]) -> BaseException | None:
-        """Run given coroutine in eventloop. It returns exceptions caught during
-        execution if any."""
-        error = None
+    def run_main(self,
+        coro: Callable[P, Coroutine[Any, Any, R]],
+        *args: P.args,
+        **kwargs: P.kwargs,
+    ) -> BaseException | None:
+        """Runs given coroutine in eventloop until finishes. It returns exceptions
+        caught during execution if any."""
         asyncio.set_event_loop(self.loop)
+        if threading.current_thread() is threading.main_thread():
+            for sig in self.signals:
+                self.loop.add_signal_handler(sig, lambda: self.signal_handler(sig))
         self.loop.set_exception_handler(self.exception_handler)
-        self._stop_event.clear()
+        if self._stop_event.done():
+            self._stop_event = Future()
         try:
-            asyncio.run(coro)
+            self.loop.run_until_complete(coro(*args, **kwargs))
         except BaseException as ex:
-            error = ex
-        return error
+            return ex
+        return None
 
-    async def execute(self) -> List[Future[Coroutine[Any, Any, Any] | BaseException]]:
-        """This executes tasks and blocks the thread until stopped."""
-        running_tasks = [self.create_task(task) for task in self.initial_tasks()]
-        await self._stop_event.wait()
-        for task in running_tasks:
+    async def main(self) -> List[Future[R | BaseException]]:
+        """It processes eventloop tasks until stopped."""
+        for name, fn in self.initial_tasks():
+            self.create_task(fn(), name)
+        await asyncio.wrap_future(self._stop_event)
+        tasks = [
+            task for task in asyncio.all_tasks()
+            if task is not asyncio.current_task()
+        ]
+        for task in tasks:
             task.cancel()
-        return await asyncio.gather(*running_tasks, return_exceptions=True)
+        return await asyncio.gather(*tasks, return_exceptions=True)
 
     def schedule(self, coro: Coroutine[Any, Any, Any]) -> Future[Any]:
         """Runs coroutine from other threads.."""
@@ -66,16 +96,22 @@ class EventLoop:
         """Run coroutine in eventloop. Must be called from same thread."""
         return self.loop.create_task(coro, name=name)
 
-    def stop_eventloop(self):
-        """This signals the eventloop to close."""
-        self._stop_event.set()
+    def stop_main(self):
+        """This signals the `main` coroutine to finish."""
+        if not self._stop_event.done():
+            self._stop_event.set_result(None)
 
-    def initial_tasks(self) -> Set[Coroutine[Any, Any, Any]]:
-        """Provides a set of initial tasks."""
-        return set()
+    def signal_handler(self, sig: int | None = None):
+        """Handle the received signal."""
+        self.stop_main()
 
     def exception_handler(self,
         loop: asyncio.AbstractEventLoop,
         context: Dict[str, Any],
     ):
         """Handle exception in running task."""
+        self.stop_main()
+
+    def initial_tasks(self) -> Set[Tuple[str | None, Callable[[], Coroutine[Any, Any, None]]]]:
+        """Provides a set of initial tasks."""
+        return set()

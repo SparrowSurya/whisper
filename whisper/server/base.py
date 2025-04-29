a679bd9e62aec32f4ddb20b5fbf2ccac1462cf5a
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-04-29 23:08:00 +0530

    Added logging to BaseServer.

--- whisper/server/base.py
+++ whisper/server/base.py
@@ -3,6 +3,7 @@ This module provides the basic server functionality to manage clients and commun
 """
 
 from whisper.packet import Packet
+from whisper.logger import Logger
 from whisper.server.connection import ConnHandle
 from whisper.typing import (
     TcpServer as _TcpServer,
@@ -14,33 +15,40 @@ class BaseServer:
     """Base server class for communicating with clients. It provides asynchronous
     methods for reading, writing and accepting."""
 
-    def __init__(self, conn: _TcpServer):
-        """Initialise connection."""
+    def __init__(self, logger: Logger, conn: _TcpServer):
+        self.logger = logger
         self.conn = conn
 
     async def accept(self, loop: _EventLoop) -> ConnHandle:
         """Accept incoming client connections."""
         sock, address = await self.conn.accept(loop)
+        self.logger.info(f"accepted connection from {address}")
         return ConnHandle(sock, address) # type: ignore
 
     async def read(self, conn: ConnHandle, loop: _EventLoop) -> Packet:
         """Read `n` bytes from connection."""
         reader = lambda n: self.conn.read(conn.sock, n, loop)  # noqa: E731
-        return await Packet.from_stream(reader)
+        packet = await Packet.from_stream(reader)
+        self.logger.debug(f"received packet: {packet!r}")
+        return packet
 
     async def write(self, conn: ConnHandle, packet: Packet, loop: _EventLoop):
         """Write `data` to connection."""
         data = packet.to_stream()
-        return await self.conn.write(conn.sock, data, loop)
+        await self.conn.write(conn.sock, data, loop)
+        self.logger.debug(f"sent packet: {packet!r}")
 
     def close(self, conn: ConnHandle):
         """Close the connection."""
+        self.logger.info(f"closed connection with {conn.address}")
         conn.sock.close()
 
     def start_server(self, host: str, port: int):
         """Start the server on given address."""
         self.conn.start(host, port)
+        self.logger.info(f"server running at {(host, port)}")
 
     def stop_server(self):
         """Close the server."""
         self.conn.stop()
+        self.logger.info("server closed")

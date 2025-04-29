7632ac28174eac38f5ae4d10b28fae846af59692
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-04-30 00:42:11 +0530

    Seperate logfiles for client and server. Converted default handlers into functions.

--- whisper/server/__main__.py
+++ whisper/server/__main__.py
@@ -5,21 +5,21 @@ It provides command line interface for server.
 import sys
 import logging
 
-from whisper.settings import APP_NAME
+from whisper.settings import APP_NAME, LOG_DIR
 from whisper.logger import Logger, stdout_handler, file_handler
 from .backend import Server
 from .tcp import TcpServer
 from .cli import get_parser
 
 
-PROGRAM = f"{APP_NAME}.server"
-
-parser = get_parser(PROGRAM, "whisper.server cli application")
+program = f"{APP_NAME}.server"
+parser = get_parser(program, "whisper.server cli application")
 args = parser.parse_args(sys.argv[1:])
 
-log_handlers = [stdout_handler, file_handler]
+logfile = LOG_DIR / "server.log"
+log_handlers = [stdout_handler(), file_handler(logfile)]
 logger = Logger(APP_NAME, logging.DEBUG, log_handlers) # type: ignore
-logger.debug(f"{PROGRAM}: {args}")
+logger.debug(f"{program}: {args}")
 
 server = Server(logger=logger, conn=TcpServer())
 server.run(host=args.host, port=args.port)

857eddf1dd267c7a04a720950c723616462182b9
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-05-02 00:10:34 +0530

    Ensuring module packet registeration before server runs.

--- whisper/server/__main__.py
+++ whisper/server/__main__.py
@@ -7,6 +7,7 @@ import logging
 
 from whisper.settings import APP_NAME, LOG_DIR
 from whisper.logger import Logger, stdout_handler, file_handler
+from whisper.packet import PacketRegistery
 from .backend import Server
 from .tcp import TcpServer
 from .cli import get_parser
@@ -18,8 +19,14 @@ args = parser.parse_args(sys.argv[1:])
 
 logfile = LOG_DIR / "server.log"
 log_handlers = [stdout_handler(), file_handler(logfile)]
-logger = Logger(APP_NAME, logging.DEBUG, log_handlers) # type: ignore
+logger = Logger(APP_NAME, logging.DEBUG, log_handlers)
 logger.debug(f"{program}: {args}")
 
-server = Server(logger=logger, conn=TcpServer())
-server.run(host=args.host, port=args.port)
+try:
+    for module in PacketRegistery.ensure_regisered():
+        logger.debug(f"dynamically imported {module.__name__}")
+except Exception as ex:
+    logger.exception(f"error occured while ensuring packet registeration: {ex}")
+else:
+    server = Server(logger=logger, conn=TcpServer())
+    server.run(host=args.host, port=args.port)

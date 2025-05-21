a843f6d58bde0571a4aed817482f9c3a1d9dd3bd
Suryakant <92842745+SparrowSurya@users.noreply.github.com>
2025-05-21 22:09:42 +0530

    Fixed TclError in tk_setPalette call

--- whisper/client/app.py
+++ whisper/client/app.py
@@ -54,7 +54,7 @@ class App(Client, MainWindow):
         configuration."""
         self.on_window_exit(lambda: self.shutdown(ExitReason.SELF_EXIT))
         palette_opts = self.create_palette(self.setting.theme.palette)
-        self.set_palette(**palette_opts)
+        self.set_palette(self.setting.theme.palette.base, **palette_opts)
         self.set_theme(self.setting.theme)
         self.set_font(**self.setting.theme.font)
         self.setup_root()
